4e3156e205bfe886d4a0d4fbf418820f0e4f3c95
Rocío Márquez <winged_zelda@hotmail.com>
2017-08-24 22:47:01 -0500

    Sistema de Particulas de mmuerte y reaparicion del player

--- miniUnity/gamesPlusJames_tuto/Assets/Scripts/LevelManager.cs
+++ miniUnity/gamesPlusJames_tuto/Assets/Scripts/LevelManager.cs
@@ -10,6 +10,13 @@ public class LevelManager : MonoBehaviour {
 	//objeto player de la clase PlayerController
 	private PlayerController player;
 
+	//variables de control de particulas
+	public GameObject deathParticle;
+	public GameObject respawnParticle;
+
+	//variable de pausa en tiempo para reaparecer
+	public float respawnDelay;
+
 
 	// Use this for initialization
 	void Start () {
@@ -22,12 +29,35 @@ public class LevelManager : MonoBehaviour {
 		
 	}
 
-	//metodo para reaparecer
+	//metodo para controlar tiempo del reaparecimiento
 	public void RespawnPlayer()
 	{
-		//texto en consola
+		//manda a llamar un metodo en IEnumerator
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	//metodo para reaparecer que esta en un IEnumerator para controlar su tiempo
+	public IEnumerator RespawnPlayerCo()
+	{
+		//aqui se manda a llamar las particulas de muerte, donde esta posicionado el player, con su rotacion del player
+		Instantiate (deathParticle, player.transform.position, player.transform.rotation);
+		//ya que hay un retraso de tiempo para reaparecer, el player sigue siendo visible en es lapso y puede moverse
+		//deshabilitaremos en ese tiempo al player para corregirlo
+		player.enabled = false;
+		player.GetComponent<Renderer> ().enabled = false;
+		//texto en consola de que ha reaparecido el player
 		Debug.Log ("Player Respawn");
+
+		//funcion para realizar un retardo entre lo que esta arriba y debajo de ella
+		yield return new WaitForSeconds (respawnDelay);
+
 		//la posicion del "player" será en la posicion de currentCheckPoint
 		player.transform.position = currentCheckpoint.transform.position;
+		//antes hemos deshabilitado el player, ya que ha reaparecido vamos a habilitarlo
+		//y hacerlo visible
+		player.enabled = true;
+		player.GetComponent<Renderer> ().enabled = true;
+		//aqui se manda a llamar las particulas de reaparecer, donde se encuentra el checkpoint activo, con su rotacion del checkpoint
+		Instantiate (respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
 	}
 }
