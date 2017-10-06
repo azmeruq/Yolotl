5270112bf8bcfb2b2043e87801b14902a2ced867
Yas HB <daughterofthewind10@gmail.com>
2017-10-05 23:13:19 -0500

    Plataformas han sido agregadas, se necesita corregir el manejador de dialogos para que se puedan integrar las cinematicas

--- Prototipos/yoloAhoraSi/Assets/Scripts/Feet.cs
+++ Prototipos/yoloAhoraSi/Assets/Scripts/Feet.cs
@@ -11,30 +11,65 @@ using UnityEngine;
 public class Feet : MonoBehaviour {
 	// Box Size than it will check if the palyer is grounded or nor
 	private float feetBoxWidth = 0.9f;
-	private float feetBoxHeight = 0.04f;
+	private float feetBoxHeight = 0.02f;
 	// Position where the box is
 	private Transform feetPosition;
+	private Player player;
 	/* 
 	 * Layer where it will compare if the box is overlaped with the
 	 * ground or not
 	*/
-	public LayerMask whatIsGround;
+	private LayerMask whatIsGround;
 
 	/*
 	 * This method draws a box in order to guide us about it size
 	 * otherwise we will not be able to see what is overlaping
 	 * and what is not.
 	*/
+
+	void Start(){
+		player = FindObjectOfType<Player> ();
+
+		feetPosition = GetComponent<Transform> ();
+	}
+
 	public void OnDrawGizmos(){
 		feetPosition = GetComponent<Transform> ();
 		Gizmos.DrawWireCube (feetPosition.position, new Vector3(feetBoxWidth, feetBoxHeight, 0f));
 	}		
 
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Platform")) {
+			player.transform.parent = other.gameObject.transform;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Platform")) {
+			player.transform.parent = null;
+		}
+	}
+
 	/*
 	 * Checks if the box is overlaping with what is on the laye Ground
 	*/
-	public bool IsOverlaping(){
-		feetPosition = GetComponent<Transform> ();
-		return Physics2D.OverlapBox (new Vector2(feetPosition.position.x, feetPosition.position.y), new Vector2(feetBoxWidth, feetBoxHeight), 360.0f, whatIsGround);
+	public void IsOverlaping(){
+		bool isOverlapingAux;
+		//feetPosition = GetComponent<Transform> ();
+
+		isOverlapingAux = OverLapingMask("Ground");
+		player.SetIsGrounded (isOverlapingAux);
+		if (isOverlapingAux) {
+			player.SetIsJumping (false);
+			//player.SetAnimationState (0);
+		}
 	}
+		
+
+	public bool OverLapingMask(string Mask){
+		whatIsGround = LayerMask.GetMask (Mask);
+		return  Physics2D.OverlapBox (new Vector2(feetPosition.position.x, feetPosition.position.y), new Vector2(feetBoxWidth, feetBoxHeight), 360.0f, whatIsGround);
+	}
+
+
 }
