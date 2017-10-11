7a0d3c0a87b2651478017d9d21ca7604efd79eaf
Yas HB <daughterofthewind10@gmail.com>
2017-10-10 23:13:29 -0500

    Juego actualizado

---
+++ Prototipos/yoloAhoraSi/Assets/Scripts/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+	private float velocidadHorizontal;
+	private Rigidbody2D rb;
+	private Player jugador;
+	private float tiempoParaDestruirse;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		jugador = FindObjectOfType<Player> ();
+		AsignarVelocidad ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2(velocidadHorizontal, rb.velocity.y);
+	}
+
+	public void AsignarVelocidad(){
+		if(jugador.GetIsFacingRight()){
+			velocidadHorizontal = 7f;
+		}else{
+			velocidadHorizontal = -7f;
+		}
+	}
+}
