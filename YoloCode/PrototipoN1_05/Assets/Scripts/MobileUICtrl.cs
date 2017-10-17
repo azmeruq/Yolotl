3bf1a756db5fa7ca452f56131dfd5378964dc3f5
Yas HB <daughterofthewind10@gmail.com>
2017-10-17 09:52:37 -0500

    Nivel uno revision

---
+++ YoloCode/PrototipoN1_05/Assets/Scripts/MobileUICtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Routes the mobile input to the correct methods in the player script
+/// </summary>
+public class MobileUICtrl : MonoBehaviour {
+	public GameObject player;
+	Player playerCtrl;
+	// Use this for initialization
+
+	/*
+	 * Obtain the components and the baheviors of the class Player
+	*/
+	void Start () {
+		playerCtrl = player.GetComponent<Player> ();
+	}
+
+	/*
+	 * Functions than 
+	*/
+	public void MobileMoveLeft(){
+		playerCtrl.MobileMoveLeft ();
+	} 
+
+	public void MobileMoveRight(){
+		playerCtrl.MobileMoveRight ();
+	}
+
+	public void MobileMoveStop(){
+		playerCtrl.MobileMoveStop ();
+	}
+
+	public void MobileMoveJump(){
+		playerCtrl.MobileMoveJump ();
+	}
+}
