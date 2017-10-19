e7d14c34493e9ab6927679e59e6674cd2f0b1525
Rocío <winged_zelda@hotmail.com>
2017-10-19 08:48:30 -0500

    diagrama de estados, npc clase en prototipo05, imagenes de doc. diseño faltantes

---
+++ YoloCode/PrototipoN1_05/Assets/Scripts/NPC.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour {
+
+	public Player player;
+	public GameObject symbol;
+	//public Text dText;
+
+	public string dialogue;
+	public bool dialogueActivated;
+	public bool playerIsClose;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Player> ();
+		setDialogue (dialogue);
+		//dialogue = " ";
+		dialogueActivated = false;
+		playerIsClose = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void setDialogue(string dialogue){
+		dialogue = dialogue;
+	}
+
+	string getDialogue(){
+		return dialogue;
+	}
+
+	void showSymbol(){
+		/*
+		 * Activar simbolo sobre cabeza de NPC
+		*/
+		symbol.SetActive (true);
+	}
+
+	void hideSymbol(){
+		/*
+		 *  Desactivar simbolo sobre cabeza de NPC
+		*/
+		symbol.SetActive (false);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player" )
+		{
+			showSymbol();
+			Debug.LogError("se puede activar mi dialogo");
+		} 
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player" )
+		{
+			//dialogueActivated = true;
+
+			hideSymbol ();
+		} 
+	}
+
+
+}
