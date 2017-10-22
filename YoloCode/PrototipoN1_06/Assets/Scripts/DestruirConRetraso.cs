62f3ab92036b35df598ad9d089f84f536d0c6abb
Rocío <winged_zelda@hotmail.com>
2017-10-22 01:16:15 -0500

    hice prototipoN1_06 (dialogo) completo, debido a peticion de github de un merge con la version prototipoN1_05

---
+++ YoloCode/PrototipoN1_06/Assets/Scripts/DestruirConRetraso.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirConRetraso : MonoBehaviour {
+	private float retrazo;
+	void Start () {
+		retrazo = 1.5f;
+		Destroy (gameObject, retrazo);	
+	}
+}
