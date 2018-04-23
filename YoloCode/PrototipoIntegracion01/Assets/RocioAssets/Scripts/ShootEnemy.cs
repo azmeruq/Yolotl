72dc3c67ccf9dbe6be795dd08aae772c20005f3f
Yas HB <daughterofthewind10@gmail.com>
2018-04-23 02:28:44 -0500

    Correcion de algunos detalles de la version integrada luego de generar apk

---
+++ YoloCode/PrototipoIntegracion01/Assets/RocioAssets/Scripts/ShootEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEnemy : MonoBehaviour {
+	public GameObject enemy_shot;
+	public Transform LaunchPoint;
+	public float waitBetweenShots;
+	private float shotCounter;
+
+	// Use this for initialization
+	void Start () {
+		shotCounter = waitBetweenShots;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shotCounter -= Time.deltaTime;
+		if (shotCounter < 0) 
+		{
+			Instantiate(enemy_shot, LaunchPoint.position, LaunchPoint.rotation);
+			shotCounter = waitBetweenShots;
+		}
+
+	}
+}
